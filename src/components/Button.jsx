b2bfd73a97c182038c7030f2dd1b69125604fd8d
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-11 22:33:47 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useStateContext } from '../contexts/contextProvider';
 
-function Button() {
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+  const { setIsClicked, initialState } = useStateContext();
+
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      onClick={() => setIsClicked(initialState)}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {icon} {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
