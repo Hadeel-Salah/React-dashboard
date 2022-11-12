5204b794c68694050db013ab6cea78891311ffa2
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-12 22:11:52 +0200

    Update contextProvider.js

--- src/contexts/contextProvider.js
+++ src/contexts/contextProvider.js
@@ -14,16 +14,29 @@ const initialState = {
 }
 
 export const ContextProvider = ({ children }) => {
-    const [isClicked, setIsClicked] = useState(initialState);
-    const [activeMenu, setActiveMenu ]= useState(true);
-    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
     const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(true);
+    const [activeMenu, setActiveMenu] = useState(true);
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+    const [isClicked, setIsClicked] = useState(initialState);
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+    };
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+    };
 
     return (
-        <StateContext.Provider value={{screenSize,setScreenSize,handleClick,activeMenu,setActiveMenu,isClicked,setIsClicked}}>
+        <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
             {children}
-        </StateContext.Provider >
-    )
-}
+        </StateContext.Provider>
+    );
+};
 
 export const useStateContext = () => useContext(StateContext);
